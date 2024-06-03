1c14462b48049722f716021d523869549595f8ce
Hascher <hascher.mc7@gmail.com>
2024-06-03 23:39:52 +0500

    Update changes

---
+++ app/src/main/java/com/githcode/magiccamera/owcamera/MyDebug.java
@@ -0,0 +1,8 @@
+package com.githcode.magiccamera.owcamera;
+
+/** Global constant to control logging, should always be set to false in
+ *  released versions.
+ */
+public class MyDebug {
+    public static final boolean LOG = false;
+}
